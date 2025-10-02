bf1ae2e4c9b24f3ca4c724f0bb56b36402e8f89a
horxx1 <egor07106+github@gmail.com>
2025-10-02 03:01:54 +0300

    Create a plus b plus c multiply 2.cs

---
+++ C#/a plus b plus c multiply 2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число: ");
+            double q = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число: ");
+            double w = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите третье число: ");
+            double e = Convert.ToDouble(Console.ReadLine());
+
+            double r = (q + w + e) * 2;
+
+            Console.WriteLine(q + " + " + w + " + " + e + " * 2");
+            Console.WriteLine("Ответ: " + r);
+        }
+    }
+}
