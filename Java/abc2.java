600894cd1bd0d8fc9e9d9ad7b8aafeb743252b12
horxx1 <egor07106+github@gmail.com>
2025-10-14 23:22:59 +0300

    add java folder and abc2.java file

---
+++ Java/abc2.java
@@ -0,0 +1,23 @@
+package Java;
+
+import java.util.Scanner;
+
+public class abc2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in, "UTF-8");
+
+        System.out.print("Введите число a: ");
+        double a = scanner.nextDouble();
+
+        System.out.print("Введите число b: ");
+        double b = scanner.nextDouble();
+
+        System.out.print("Введите число c: ");
+        double c = scanner.nextDouble();
+
+        double d = (a + b + c) * 2;
+        System.out.println("(" + a + " + " + b + " + " + c + ")" + " * 2 = " + d);
+
+        scanner.close();
+    }
+}
